f0b08a4e1cd27b55266ba8748d816a6fe3792911
JYKingWY <wyjyking@gmail.com>
2019-12-30 23:33:21 +0800

    first

---
+++ src/main/java/com/wy/concurrencylearning/ConcurrencyLearningApplication.java
@@ -0,0 +1,13 @@
+package com.wy.concurrencylearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyLearningApplication.class, args);
+	}
+
+}
