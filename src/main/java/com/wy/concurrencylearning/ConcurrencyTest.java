53f7e01980fafc7d34f92d9f6a858ded8caf7b67
JYKingWY <wyjyking@gmail.com>
2019-12-30 23:58:00 +0800

    modified some annotations added a not thread safe class

---
+++ src/main/java/com/wy/concurrencylearning/ConcurrencyTest.java
@@ -0,0 +1,49 @@
+package com.wy.concurrencylearning;
+
+import com.wy.concurrencylearning.annotation.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+@NotThreadSafe
+public class ConcurrencyTest {
+
+    // 请求总数
+    public static final int clientTotal = 5000;
+
+    // 同时并发执行的线程数
+    public static final int threadTotal = 200;
+
+    public static int count = 0;
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    log.info("error: ", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{} ",count);
+    }
+
+    private static void add() {
+        count++;
+    }
+
+
+}
--- src/main/java/com/wy/concurrencylearning/annotation/NotRecommend.java
+++ src/main/java/com/wy/concurrencylearning/annotation/NotRecommend.java
@@ -11,6 +11,6 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface UnRecommend {
+public @interface NotRecommend {
     String value() default "";
 }
